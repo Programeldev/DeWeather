425962848a77c0be2232535a1675364541e5bf8a
Programeldev <wojtek.deren@spoko.pl>
2024-10-17 15:57:27 +0200

    Scrolling between sections with wheel mouse testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'DeWeather';
+  switchComponent = false;
+
+  @HostListener('wheel', ['$event'])
+  public onScroll(event: WheelEvent) {
+    if(event.deltaY) {
+      if(this.switchComponent) {
+        d = document.getElementById('d1');
+        d.scrollIntoView({behavior: 'smooth'});
+        this.switchComponent = false;
+      }
+      else {
+        d = document.getElementById('d2');
+        d.scrollIntoView({behavior: 'smooth'});
+        this.switchComponent = true;
+      }
+    }
+  }
 }
